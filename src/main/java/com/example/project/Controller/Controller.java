0397627d823b645075f2645e2f505b6e98c5cf5e
abdelkadermiladi <96490190+abdelkadermiladi@users.noreply.github.com>
2023-06-20 16:04:18 +0200

    update with angular response

--- src/main/java/com/example/project/Controller/Controller.java
+++ src/main/java/com/example/project/Controller/Controller.java
@@ -5,9 +5,15 @@ import com.example.project.Model.JenkinsJobBuild;
 import com.example.project.Service.JenkinsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -20,21 +26,42 @@ public class Controller {
     public Controller(JenkinsService jenkinsService) {
         this.jenkinsService = jenkinsService;
     }
+
     @GetMapping("/last-job-build")
-    public String getLastJobName() {
+    public ResponseEntity<Object> getLastJobDuration() {
         try {
             JenkinsJobBuild jobBuild = jenkinsService.getLatestJobBuild();
             if (jobBuild != null) {
-                return jobBuild.getJobName();
+                String duration = "Last Job Duration : " + jobBuild.getjobDuration() + " milliseconds";
+                Map<String, String> response = new HashMap<>();
+                response.put("duration", duration);
+                return ResponseEntity.ok().body(response);
             } else {
-                return "No job build found.";
+                return ResponseEntity.ok().body(Collections.singletonMap("message", "No job build found."));
             }
         } catch (JsonProcessingException e) {
-            return "Error processing the Jenkins job build data.";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("error", "Error processing the Jenkins job build data."));
         } catch (Exception e) {
-            return "An unexpected error occurred: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("error", "An unexpected error occurred: " + e.getMessage()));
         }
     }
+
+
+    //@GetMapping("/last-job-build")
+    //public String getLastJobDuration() {
+    //    try {
+    //        JenkinsJobBuild jobBuild = jenkinsService.getLatestJobBuild();
+    //        if (jobBuild != null) {
+    //            return ("Last Job Duration : "+jobBuild.getjobDuration()+" millsconds");
+    //        } else {
+    //            return "No job build found.";
+    //        }
+    //     } catch (JsonProcessingException e) {
+    //        return "Error processing the Jenkins job build data.";
+    //    } catch (Exception e) {
+    //        return "An unexpected error occurred: " + e.getMessage();
+    //    }
+    //}
 }
 
 
